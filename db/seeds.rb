beb9ff0dd5ad4a3ee5cd58a2cd73bef1ff0bec36
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-08-08 19:54:42 +0200

    add models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |number|
+    p "User ##{number + 1} created"
+    User.create!(email: Faker::Internet.email)
+end
+
+1000.times do |number|
+    p "Product ##{number + 1} created"
+    Product.create!(name: Faker::Appliance.equipment,
+                    price: (10..100000).to_a.sample,
+                    quantity: (5..100).to_a.sample)
+end
