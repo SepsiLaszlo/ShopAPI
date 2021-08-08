97e89bd1b17509c4d0b2570f7968a24b953f9592
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-08-08 20:46:02 +0200

    add finalize to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_products
+
+  def add(product:, quantity:)
+    raise "Order is already finalized!" if finalized?
+
+    OrderProduct.create!(product: product,
+                         quantity: quantity,
+                         order: self )
+  end
+
+  def finalize!
+
+  end
 end
