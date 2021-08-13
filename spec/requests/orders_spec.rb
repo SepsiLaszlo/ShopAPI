e6ed45ef48c43bbecbe041aa5aba4697a24c0f3a
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-08-13 21:54:30 +0200

    rename spec

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "Orders", type: :request do
   before(:each) do
     User.create!(email: 'admin@test.com')
+
     User.create!(email: 'customer@test.com')
 
     Product.create!(name: 'cinnamon roll',
@@ -14,7 +15,7 @@ RSpec.describe "Users", type: :request do
                     quantity: 10)
   end
 
-  it 'login' do
+  it 'calculates the correct total_price' do
     post '/users/2/login'
 
     post '/products/1/add_to_order', params: { quantity: 2 }
